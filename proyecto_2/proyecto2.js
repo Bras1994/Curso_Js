f9b45167a5b7187a757b62f2dd688ceaa68545bc
Agustina <81659547+Bras1994@users.noreply.github.com>
2023-01-22 15:40:29 -0300

    Add files via upload

---
+++ proyecto_2/proyecto2.js
@@ -0,0 +1,15 @@
+const nombre= ["Brenda Santos"]
+const edad= 29
+let desarrollador= true
+let fecha_nacimiento= new Date(1994,0,9)
+
+const libro={
+    titulo: "Harry Potter y el prisionero de Azcaban",
+    fecha_de_lanzamiento: 1999,
+    autora: "J. K. Rowling",
+    genero: 'Literatura fantástica'
+}
+
+const sombre_mi= [nombre, edad, desarrollador,fecha_nacimiento,libro]
+
+console.log(sombre_mi)
